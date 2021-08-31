919e0d58fd21e133e02152b0146fdec162240d8d
Strozynski <Strozynskiw@gmail.com>
2021-08-31 10:14:25 +0200

    Remove an empty line at the end of an output file

--- src/main.rs
+++ src/main.rs
@@ -66,11 +66,11 @@ fn parse(path: &str) -> Result<Vec<Transaction>> {
 }
 
 fn print_map(result: &engine::AccountsMap) -> Result<()> {
-    println!("client,available,held,total,locked");
+    print!("client,available,held,total,locked");
 
     result.iter().for_each(|(c, d)| {
-        println!(
-            "{},{},{},{},{}",
+        print!(
+            "\n{},{},{},{},{}",
             c,
             money_to_string(&d.available),
             money_to_string(&d.held),
